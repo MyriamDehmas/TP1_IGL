237451a932f310bb30c0b520223426086eec494d
MyriamDehmas <fm_dehmas@esi.dz>
2017-10-24 16:40:31 -0100

    Test Methods Update

--- VectorHelperTest.java
+++ VectorHelperTest.java
@@ -1,36 +1,41 @@
 import static org.junit.Assert.*;
-
+import java.util.Arrays;
 /**
  * Created by dellpc on 18/10/2017.
  */
-public class VectorHelperTest {
+   public class VectorHelperTest {
+    private VectorHelper vh= new VectorHelper();
     @org.junit.Test
-    public void reverse() throws Exception {
-    int testArray[]= new int [6];
-        for(int i= 0; i< testArray.length; i++)
-        {
-            testArray[i]= i;
+    public void reverseTest() throws Exception {
 
-        }
-
-    int [] expectedArray= {5,4,3,2,1,0} ;
-        reverse(testArray);
-    }
-
-    @org.junit.Test
-    public void minMax() throws Exception {
+        int [] enteryVect = { 1,2,3,4,5,6};
+        int [] expectedResult = { 6,5,4,3,2,1};
+        int [] effectiveResult= vh.reverse(enteryVect);
+        if ( !Arrays.equals(expectedResult,effectiveResult) )     { fail("inverse method doesn't work");}
 
     }
 
     @org.junit.Test
-    public void doubleArray() throws Exception {
+    public void minMaxTest() throws Exception {
 
+        int [] enteryVect = { 7,-2,3,8,5,1};
+        int [] expectedResult = {-2,8};
+        int [] effectiveResult= vh.minMax(enteryVect);
+        if ( !Arrays.equals(expectedResult,effectiveResult) )     { fail("minMax method doesn't work");}
     }
 
- //--------------------------------------------------
+    @org.junit.Test
+    public void doubleArrayTest() throws Exception {
+
+        int [] enteryVect = { 0,1,2,3,4,-20};
+        int [] expectedResult = {0,2,4,6,8,-40};
+        int [] effectiveResult= vh.doubleArray(enteryVect);
+        if ( !Arrays.equals(expectedResult,effectiveResult) )     { fail("doubleArray method doesn't work");}
+    }
+
+    //--------------------------------------------------
     //Added by Lina
 
-    VectorHelper v = new VectorHelper();
     @org.junit.Test
     public void SommeTest() throws Exception {
         int vect3[] = {1, 2, 3};
@@ -38,7 +43,7 @@ public class VectorHelperTest {
         int result1 [] = new int[3];
         int expected[] = {5, 10, 5};
 
-        v.Somme(vect4, vect3, result1);
+        vh.Somme(vect4, vect3, result1);
         assertArrayEquals(expected,result1);
 
 
@@ -51,7 +56,7 @@ public class VectorHelperTest {
 
         int expected[] = {2, 5, 6, 9, 10};
 
-        v.Tri(vect1);
+        vh.Tri(vect1);
         assertArrayEquals(vect1, expected);
 
 
