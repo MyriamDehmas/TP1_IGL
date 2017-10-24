b256c573259667b6a6bacd8a521da83420503cbd
Lina-Wissem <32876501+Lina-Wissem@users.noreply.github.com>
2017-10-24 18:47:34 +0100

    Add files via upload

---
+++ DifferenceException.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Nae on 23/10/2017.
+ */
+public class DifferenceException extends Exception{
+
+    //System.out.println("Les tailles des deuc vecteurs sont différentes, donc l'opération de somme est impossible !");
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
