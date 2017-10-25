736b6e9f7a0b6d573aa9c3a86d3de5a5994c97f7
Lina-Wissem <fl_mouss@esi.dz>
2017-10-25 01:40:44 +0100

    Main changed
    Case 2 added

--- src/Main.java
+++ src/Main.java
@@ -72,6 +72,37 @@ public class Main {
                     break;
 
 
+
+
+                case 2:
+
+                    System.out.println(" Enter th length of the array 1 :"); tai = sc.NextInt();
+                    array = new int[tai];
+                    for (int i=0; i<tai; i++)
+
+                    {
+                        System.out.println(" array[" + i + "]");
+                        array[i] = sc.NextInt();
+                    }
+
+                    System.out.println(" Enter th length of the array 2 :"); tai = sc.NextInt();
+                    array2 = new int[tai];
+                    for (int i=0; i<tai; i++)
+
+                    {
+                        System.out.println(" array2[" + i + "]");
+                        array2[i] = sc.NextInt();
+                    }
+                   int result [tai]= vectHelp.Somme(array , array2);
+                    System.out.println(" Here is the array result :");
+                    for (int i=0; i<tai ; i++)
+                    {
+                        System.out.println(" array[" +i +"] =" + result[i]);
+                    }
+
+                    break;
+
+
             }
         }
 
